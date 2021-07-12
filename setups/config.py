6dbc941632f002e66619ada19278a7a9e3eb127f
nop-0 <79671740+nop-0@users.noreply.github.com>
2021-07-12 16:54:10 +0800

    Add files via upload

--- setups/config.py
+++ setups/config.py
@@ -1,9 +1,9 @@
-MODEL_PATH = "yolo_coco"
+MODEL_PATH = "yolo_coco" #points algorithm to YOLOv3 dataset
 
-MIN_CONF = 0.3
-NMS_THRESH = 0.3
+MIN_CONF = 0.3  #threshold of detection confidence precentage (%30)
+NMS_THRESH = 0.3 #threshold of Non-Max Suppression (for overlapping bbox)
 
-USE_GPU = True
+USE_GPU = True #usage of Graphical Processing Unit for video processing
 
-MIN_DISTANCE = 100
+MIN_DISTANCE = 100 #set minimum distance
 
--- setups/detect.py
+++ setups/detect.py
@@ -13,6 +13,58 @@ def detect_people(frame, net, ln, personIDx = 0):
     layerOutputs = net.forward(ln)
     
     boxes = []
+    centroids = []
+    confidences = []
+    
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            
+            if classID == personIDx and confidence > MIN_CONF: #30% confidence
+            
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                #obtain x-y coordinates of individuals
+                x = int(centerX - (width/2))
+                y = int(centerY - (height/2))
+                
+                boxes.append([x, y, int(width), int(height)])
+                centroids.append((centerX, centerY))
+                confidences.append(float(confidence))
+                
+    #apply Non-Max Suppression for overlapping bounding box
+    IDxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+
+    if len(IDxs) > 0:
+        for i in IDxs.flatten():
+            #combines x,y, width and height of the individuals 
+            (x,y) = (boxes[i][0], boxes[i][1])
+            (w,h) = (boxes[i][2], boxes[i][3])
+            
+            #combines data
+            r = (confidences[i], (x, y, x+w, y+h), centroids[i])
+            results.append(r)
+            
+    return results
+    
+    
+    
+    
+    
+    
+
+def detect_people2(frame, net, ln, personIDx = 0):
+    #obtain frame from video, net from YOLO model, YOLO CNN layer, index for YOLO detection
+    (H,W) = frame.shape[:2]
+    results = []
+    
+    blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416,416), swapRB = True, crop = False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+    
+    boxes = []
 
     centroids = []
     confidences = []
